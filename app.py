a4489dac3f29fbf56644ab3d55f16f7944338312
Konstantin Lipkin <konstantinlipkin@localhost.localdomain>
2020-11-01 11:46:38 +0300

    foo

--- app.py
+++ app.py
@@ -18,17 +18,13 @@ def hc():
 
 
 @app.route('/api/recommend', methods=['POST'])
-def v1_image_recognition():
+def recommendation():
     current_time = datetime.datetime.now()
-    result = ImageRecognition(request.json).classify()
+    result = Recommendation(request.json).classify()
     print(
         f'TOTAL: {datetime.datetime.now() - current_time}', file=sys.stdout)
     return jsonify({'data': result})
 
-
-@app.route('/api/recognition/v1/video', methods=['POST'])
-def v1_video_recognition():
-    return jsonify({'data': VideoRecognition(request.json).classify()})
 
 if __name__ == '__main__':
     app.run()
